aa591fa8299f339526e52c86ea4c89655399a7e5
Jesper <jesper.toft.jakobsen@gmail.com>
2022-03-09 12:24:35 +0100

    Fix circle issue with radius not being applyed

--- FigurTegneren/src/main/scala/FigurTegnerenScala.scala
+++ FigurTegneren/src/main/scala/FigurTegnerenScala.scala
@@ -20,44 +20,44 @@ object FigurTegnerenScala {
 
   //https://www.mathworks.com/matlabcentral/answers/98665-how-do-i-plot-a-circle-with-a-given-radius-and-center
   @tailrec
-  private def circle(x: Float, y: Float, r: Float, counter: Int, smoothness: Int, x_coords: List[Double], y_coords: List[Double]): List[java.util.List[Double]] = {
+  private def circle(x: Int, y: Int, r: Int, counter: Int, smoothness: Int, x_coords: List[Int], y_coords: List[Int]): List[java.util.List[Int]] = {
     if (counter == smoothness + 1) {
-      List[java.util.List[Double]](x_coords.reverse.asJava, y_coords.reverse.asJava)
+      List[java.util.List[Int]](x_coords.reverse.asJava, y_coords.reverse.asJava)
     }
     else {
-      circle(x, y, r, counter + 1, smoothness, x_coords.::(cos(((Pi * 2) / smoothness) * counter) + x), y_coords.::(sin(((Pi * 2) / smoothness) * counter) + y))
+      circle(x, y, r, counter + 1, smoothness, x_coords.::((cos(((Pi * 2) / smoothness) * counter) * r + x).asInstanceOf[Int]), y_coords.::((sin(((Pi * 2) / smoothness) * counter) * r + y).asInstanceOf[Int]))
     }
   }
 
-  def circle(x: Float, y: Float, r: Float, smoothness: Int): java.util.List[java.util.List[Double]] = {
-    circle(x, y, r, 0, smoothness, List[Double](), List[Double]()).asJava
+  def circle(x: Int, y: Int, r: Int, smoothness: Int): java.util.List[java.util.List[Int]] = {
+    circle(x, y, r, 0, smoothness, List[Int](), List[Int]()).asJava
   }
 
   @tailrec
   private def square(x1: Float, y1: Float, x2: Float, y2: Float, counter_x: Float, counter_y: Float, x_coords: List[Float], y_coords: List[Float]): List[java.util.List[Float]] = {
-    if (x_coords.length > ((x2-x1) + (y2-y1)) * 2) {
+    if (x_coords.length > ((x2 - x1) + (y2 - y1)) * 2) {
       List[java.util.List[Float]](x_coords.reverse.asJava, y_coords.reverse.asJava)
     }
     else {
-      if (y_coords.length < y2-y1) {
-        square(x1, y1, x2, y2, counter_x, counter_y+1, x_coords.::(x1), y_coords.::(counter_y))
+      if (y_coords.length < y2 - y1) {
+        square(x1, y1, x2, y2, counter_x, counter_y + 1, x_coords.::(x1), y_coords.::(counter_y))
       }
-      else if(x_coords.length < x2-x1 + y2-y1 + 1) {
-        square(x1, y1, x2, y2, counter_x+1, 0, x_coords.::(x1+counter_x), y_coords.::(y2))
+      else if (x_coords.length < x2 - x1 + y2 - y1 + 1) {
+        square(x1, y1, x2, y2, counter_x + 1, 0, x_coords.::(x1 + counter_x), y_coords.::(y2))
       }
-      else if (y_coords.length < x2-x1 + ((y2-y1)*2) + 1) {
-        square(x1, y1, x2, y2, 0, counter_y-1, x_coords.::(x2), y_coords.::(y2+counter_y-1))
+      else if (y_coords.length < x2 - x1 + ((y2 - y1) * 2) + 1) {
+        square(x1, y1, x2, y2, 0, counter_y - 1, x_coords.::(x2), y_coords.::(y2 + counter_y - 1))
       }
       else {
-        square(x1, y1, x2, y2, counter_x-1, counter_y, x_coords.::(x2+counter_x-1), y_coords.::(y1))
+        square(x1, y1, x2, y2, counter_x - 1, counter_y, x_coords.::(x2 + counter_x - 1), y_coords.::(y1))
       }
     }
   }
 
   def square(x1: Float, x2: Float, y1: Float, y2: Float): java.util.List[java.util.List[Float]] = {
     if (x1 < x2 && y1 < y2) {
-      square(x1, y1, x2, y2, x1, y1,List[Float](), List[Float]()).asJava
-    }else{
+      square(x1, y1, x2, y2, x1, y1, List[Float](), List[Float]()).asJava
+    } else {
       List[java.util.List[Float]]().asJava
     }
   }
