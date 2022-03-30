c199f612dd095706e71686d3d1ebe1c5e2a061e2
Jesper <jesper.toft.jakobsen@gmail.com>
2022-03-30 14:35:28 +0200

    Locked window to fix graphics issues

--- FigurTegneren/src/main/java/FigurTegnerenGUI.java
+++ FigurTegneren/src/main/java/FigurTegnerenGUI.java
@@ -5,13 +5,16 @@ import java.util.List;
 public class FigurTegnerenGUI extends JFrame{
 
     public FigurTegnerenGUI() throws HeadlessException {
+        final double windowWidth = 0.9;
+        final double windowHeight = 0.9;
         final double verticalDivider = 0.9;
         final double horizontalDivider = 0.75;
 
         setName("FigurTegneren");
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        setSize(new Dimension((int) (screenSize.width*0.7), (int) (screenSize.height*0.7)));
-
+        setSize(new Dimension((int) (screenSize.width*windowWidth), (int) (screenSize.height*windowHeight)));
+        setResizable(false); // IF this is not done we can resize the window fucking the drawing up!
+        // MAX SIZE LINE (0 0) (1295 862)
         final OutputConsole outputConsole = new OutputConsole();
         final GraphicsPlane graphicsPlane = new GraphicsPlane();
         final InputConsole inputConsole = new InputConsole(outputConsole, graphicsPlane);
@@ -21,13 +24,13 @@ public class FigurTegnerenGUI extends JFrame{
 
         JSplitPane HorzSplitLine = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, graphicsPlane, scrollPane);
         HorzSplitLine.setSize(getSize());
-        HorzSplitLine.setDividerLocation(0.75);
+        HorzSplitLine.setDividerLocation(horizontalDivider);
 
         JSplitPane VertSplitLine = new JSplitPane(JSplitPane.VERTICAL_SPLIT, HorzSplitLine, inputConsole);
         VertSplitLine.setSize((int) (getWidth()*horizontalDivider),getHeight());
         VertSplitLine.setDividerLocation(verticalDivider);
 
-        graphicsPlane.setSize((int)((screenSize.width*0.7)*horizontalDivider),(int)((screenSize.height*0.7)*verticalDivider));
+        graphicsPlane.setSize((int)((screenSize.width*windowWidth)*horizontalDivider),(int)((screenSize.height*windowHeight)*verticalDivider));
         graphicsPlane.SetupWindow();
 
         add(VertSplitLine);
