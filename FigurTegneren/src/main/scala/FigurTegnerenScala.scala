e07d29af2f16b83387c763351c69ae02436a0eb9
Jesper <jesper.toft.jakobsen@gmail.com>
2022-05-01 15:33:14 +0200

    Line fixed lorte bug

--- FigurTegneren/src/main/scala/FigurTegnerenScala.scala
+++ FigurTegneren/src/main/scala/FigurTegnerenScala.scala
@@ -61,6 +61,8 @@ object FigurTegnerenScala {
     val dx = (x2-x1).abs
     val dy = (y2-y1).abs
 
+
+
     if(dx >= dy) {
       if(dy != 0) {
         lineImpl(x1, x2, y1, y2, x1.toFloat, y1.toFloat, 1, dy.toFloat/dx.toFloat, dx, 1, List[Int](x1), List[Int](y1))
@@ -98,7 +100,18 @@ object FigurTegnerenScala {
       List[java.util.List[Int]]().asJava
     }
     else {
-      clipBoundingBox(boundingBox, lineRunner(x1, x2, y1, y2)).asJava
+      if (x1 < x2 && y1 <= y2) {
+        clipBoundingBox(boundingBox, lineRunner(x1, x2, y1, y2)).asJava
+      }
+      else if (x2 < x1 && y1 <= y2) {
+        clipBoundingBox(boundingBox, lineRunner(x2, x1, y1, y2)).asJava
+      }
+      else if (x1 <= x2 && y2 < y1) {
+        clipBoundingBox(boundingBox, lineRunner(x1, x2, y2, y1)).asJava
+      }
+      else {
+        clipBoundingBox(boundingBox, lineRunner(x2, x1, y2, y1)).asJava
+      }
     }
   }
 
